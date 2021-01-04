94aec972ec33bb659431d3cd0ec3ac65f1c681cf
Saurav7192 <sauravkumar71920@gmail.com>
2021-01-04 16:46:56 +0530

    scheduler

---
+++ conference-scheduling/src/main/java/com/sauravkumar71920/conferencescheduling/ConferenceSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.sauravkumar71920.conferencescheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceSchedulingApplication.class, args);
+	}
+
+}
